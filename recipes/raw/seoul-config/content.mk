545d46ce9f117dd1261d0546c35b45b1659ea498
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2024-05-25 14:55:59 +0200

    seoul: adjust packages to use usb model - WiP

--- recipes/raw/seoul-config/content.mk
+++ recipes/raw/seoul-config/content.mk
@@ -1,4 +1,5 @@
-CONFIG_FILES = vm_seoul.cfg vm_seoul_boot_0.cfg vm_seoul_boot_1.cfg
+CONFIG_FILES  = vm_seoul.cfg vm_seoul_usb.cfg
+CONFIG_FILES += vm_seoul_boot_0.cfg vm_seoul_boot_1.cfg
 CONFIG_FILES += vm_seoul_audio.cfg vm_seoul_4cpu_3disk.cfg
 
 content: $(CONFIG_FILES)
