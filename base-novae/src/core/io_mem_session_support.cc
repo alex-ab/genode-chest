d4eee468800d642091ccfa1d8d53fda59c4bf55b
Alexander Boettcher <alexander.boettcher@genode-labs.com>
2025-01-25 18:16:55 +0100

    TODO - drop when rebasing to new release

--- base-novae/src/core/io_mem_session_support.cc
+++ base-novae/src/core/io_mem_session_support.cc
@@ -7,7 +7,7 @@
  */
 
 /*
- * Copyright (C) 2009-2017 Genode Labs GmbH
+ * Copyright (C) 2009-2024 Genode Labs GmbH
  *
  * This file is part of the Genode OS framework, which is distributed
  * under the terms of the GNU Affero General Public License version 3.
@@ -16,11 +16,16 @@
 /* core includes */
 #include <io_mem_session_component.h>
 #include <platform.h>
-//#include <nova_util.h>
 
 using namespace Core;
 
 
 void Io_mem_session_component::_unmap_local(addr_t, size_t, addr_t) { }
 
-addr_t Io_mem_session_component::_map_local(addr_t, size_t) { return 0; }
+
+Io_mem_session_component::Dataspace_attr Io_mem_session_component::_map_local(addr_t const base,
+                                                                              size_t const size,
+                                                                              addr_t const req_base)
+{
+	return Dataspace_attr(size, 0, base, _cacheable, req_base);
+}
